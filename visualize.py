f1dae17d98a401cbcf3f2b433b7210d05bc3ce88
yusanshi <meet.leiyu@gmail.com>
2022-04-23 20:39:22 +0800

    Update visualizing code

--- visualize.py
+++ visualize.py
@@ -16,14 +16,13 @@
 import numpy as np
 import torch
 
-from tqdm import tqdm, trange
 from sklearn.manifold import TSNE
 from matplotlib import cm
 import matplotlib.pyplot as plt
 
 # %%
 '''
-Generating val_test_graph.pt:
+Generating user_group_graph.pt:
 
     total_graph = [
         torch.sparse_coo_tensor(size=(NUM_USER, NUM_GROUP),
@@ -37,56 +36,62 @@ Generating val_test_graph.pt:
         current_graph = torch.pow(current_graph, args.WEIGHT_SMOOTHING_EXPONENT)
         total_graph.append(current_graph + total_graph[-1] * args.DECAY_RATE)
 
-    val_test_graph = total_graph[TRAIN_NUM]
+    user_group_graph = total_graph[TRAIN_NUM]
     
 '''
-val_test_graph_path = 'val_test_graph.pt'
+user_group_graph_path = 'user_group_graph.pt'
 user_embedding_path = {
     'baseline': 'baseline.npy',
-    'gcn_2_layer': 'gcn_2_layer.npy'
+    'gcn-2-layer-0%': 'gcn-2-layer-3.npy',
+    'gcn-2-layer-17%': 'gcn-2-layer-18.npy',
+    'gcn-2-layer-33%': 'gcn-2-layer-33.npy',
+    'gcn-2-layer-50%': 'gcn-2-layer-48.npy',
+    'gcn-2-layer-67%': 'gcn-2-layer-63.npy',
+    'gcn-2-layer-83%': 'gcn-2-layer-78.npy',
+    'gcn-2-layer-100%': 'gcn-2-layer-93.npy'
 }
+num_selected_group = 10
 
 # %%
-val_test_graph = torch.load(val_test_graph_path).coalesce()
-NUM_USER, NUM_GROUP = val_test_graph.size()
+user_group_graph = torch.load(user_group_graph_path).coalesce()
+NUM_USER, NUM_GROUP = user_group_graph.size()
 
 # %%
-degrees = val_test_graph.values().numpy()
-degree_threshold = np.percentile(degrees, 90)
+degrees = user_group_graph.values().numpy()
+degree_threshold = np.percentile(degrees, 85)
 degree_threshold
 
 # %%
-val_test_graph = (val_test_graph.to_dense() >= degree_threshold).int()
-val_test_graph.sum(dim=0)
+user_group_graph = (user_group_graph.to_dense() >= degree_threshold).int()
+user_group_graph.sum(dim=0)
 
 # %%
-num_selected_group = 10
-selected_group_indexs = np.argsort(-val_test_graph.sum(
+selected_group_indexs = np.argsort(-user_group_graph.sum(
     dim=0).numpy())[:num_selected_group]
 selected_group_indexs
 
 # %%
-val_test_graph = val_test_graph[:, selected_group_indexs]
+user_group_graph = user_group_graph[:, selected_group_indexs]
 
 # %%
-selected_user_indexs = (val_test_graph.sum(
+selected_user_indexs = (user_group_graph.sum(
     dim=1) == 1).nonzero().flatten().numpy()
 selected_user_indexs
 
 # %%
-index_map = dict(val_test_graph[selected_user_indexs, :].nonzero().tolist())
+index_map = dict(user_group_graph[selected_user_indexs, :].nonzero().tolist())
 index_map
 
 # %%
-baseline_embedding = np.load(user_embedding_path['baseline'])
-gcn_2_layer_embedding = np.load(user_embedding_path['gcn_2_layer'])
-user_embeddings = {
-    'Baseline': baseline_embedding,
-    'GCN-2-layer': gcn_2_layer_embedding,
-    'GCN-2-layer-0': gcn_2_layer_embedding[:, :64],
-    'GCN-2-layer-1': gcn_2_layer_embedding[:, 64:64 * 2],
-    'GCN-2-layer-2': gcn_2_layer_embedding[:, 64 * 2:],
-}
+user_embeddings = {}
+for k, v in user_embedding_path.items():
+    if k.startswith('baseline'):
+        user_embeddings[k] = np.load(v)
+    elif k.startswith('gcn-2-layer'):
+        for i, x in enumerate(np.split(np.load(v), 3, axis=1)):
+            user_embeddings[f'{k}-{i}'] = x
+    else:
+        raise
 
 # %%
 for k, v in user_embeddings.items():
@@ -97,17 +102,14 @@ colors = cm.rainbow(np.linspace(0, 1, num_selected_group))
 for k, v in user_embeddings.items():
     print(k)
     user_embedding = v[selected_user_indexs]
-    points = TSNE(verbose=1).fit_transform(user_embedding)
+    points = TSNE().fit_transform(user_embedding)
     c = [colors[index_map[i]] for i in range(len(points))]
     label = [index_map[i] for i in range(len(points))]
-    plt.scatter(points[:, 0],
-                points[:, 1],
-                s=1.5,
-                marker='o',
-                c=c,
-                label=label)
+    plt.scatter(points[:, 0], points[:, 1], s=2, marker='o', c=c, label=label)
     plt.savefig(f'{k}.svg', dpi=200)
     plt.show()
 
 # %%
 # !scp *.svg aliyun:/var/www/img.yusanshi.com/upload/social-computing
+
+# %%
